e0d2e27985d279cba0b07b07624ebad67cdfbb19
allisson 011 <allissonkauasantosrocha@gmail.com>
2026-05-27 19:08:36 -0300

    Create tabelas.c

---
+++ tabelas.c
@@ -0,0 +1,13 @@
++ // CONJUNÇÃO:
++ // P | Q | P && Q 
++ // V | V | V
++ // V | F | F
++ // F | V | F
++ // F | F | F
+
++ // DIJUNÇÃO:
++ // P | Q | P || Q
++ // V | V | V
++ // V | F | V
++ // F | V | V
++ // F | F | F
