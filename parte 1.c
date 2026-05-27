3cca34c25c10cb2787f4ba9a6ef8c3f167d30141
allisson 011 <allissonkauasantosrocha@gmail.com>
2026-05-27 19:06:12 -0300

    Create parte 1.c

---
+++ parte 1.c
@@ -0,0 +1,17 @@
+//PENSAMENTO COMPUTACIONAL: Habilidade que permite resolver problemas dividindo-os em partes menores.
+//PENSAMENTO COMPUTACIONAL: É uma habilidade que permite resolver problemas dividindo-os em partes menores.
+
+//DECOMPOSIÇÃO: Quebrar um problema grande e complexo em partes menos e mais fáceis de resolver.
+
+//RECONHECIMENTO DE PADRÕES: Identificar semelhanças ou conexões entre o problema atual e outros já resolvidos.
+
+//ABSTRAÇÃO: Filtrar e ignorar os detalhes irrelevantes, focando apenas nas inforamçoes essênciais para resolver o problema.
+//ABSTRAÇÃO: Filtrar e ignorar os detalhes irrelevantes, focando apenas nas informações essênciais para resolver o problema.
+
+//ALGORITMOS: Criar uma sequẽncia de instruções lógicas em passo a passo para solucionar o problema.
+
+//CONJUNÇÃO X DISJUNÇÃO: 
+
+//Conjunção (AND | &&) - O resultado só é verdadeiro quando as 2 condições apresentadas são atingidas, fora isso, o resultado sempre é falso. 
+
+//Disjunção (OR | ||)- O resultado é verdadeiro quado as 2 condições apresentadas são atingidas, e também é verdadeiro quando apenas 1 das 2 condições é atingida, sendo falso só quando nenhuma delas corresponde da maneira desejada.
